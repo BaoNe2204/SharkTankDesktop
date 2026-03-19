c173c21aae28ec78c3676444849d34de380eec60
BaoNeee24 <minhtenbao24@gmail.com>
2026-03-19 23:16:06 +0700

    Update Thông Báo Hệ Thống beta

---
+++ DAL/INotificationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SharkTank.Core.Models;
+
+namespace SharkTank.DAL
+{
+    public interface INotificationRepository
+    {
+        IEnumerable<SystemNotification> GetAll();
+        IEnumerable<SystemNotification> GetActiveForUser(string roleName, string username);
+        SystemNotification GetById(int id);
+        void Insert(SystemNotification notification);
+        void Update(SystemNotification notification);
+        void Delete(int id);
+    }
+}
