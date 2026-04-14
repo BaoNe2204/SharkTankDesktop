7be75d0b32a396f18d6b61e5f7e16c9671cbe1d2
Theanne <thean28072006@gmail.com>
2026-04-14 11:08:09 +0700

    Theo dõi doanh thu (#54)
    * Thêm xóa sửa KH và Phân loại khách hàng
    
    * Lịch sử và công nợ KH p1
    
    * Quản lý bán hàng - Báo giá P1
    
    * update
    
    * Hóa đơn demo lần 1
    
    * update thư viện
    
    * fix lỗi
    
    * Theo dõi doanh thu
    
    * Theo dõi doanh thu
    
    ---------
    
    Co-authored-by: BaoNeee24 <minhtenbao24@gmail.com>

---
+++ Modules/Sales/BLL/DoanhThu_BLL.cs
@@ -0,0 +1,25 @@
+﻿using System.Data;
+using SharkTank.Modules.Sales.DAL;
+
+namespace SharkTank.Modules.Sales.BLL
+{
+    public class DoanhThu_BLL
+    {
+        private DoanhThu_DAL dal = new DoanhThu_DAL();
+
+        public DataTable LayDoanhThuTheoNgay()
+        {
+            return dal.GetDoanhThuTheoNgay();
+        }
+
+        public DataTable LayDoanhThuTheoNhanVien()
+        {
+            return dal.GetDoanhThuTheoNhanVien();
+        }
+
+        public DataTable LayDoanhThuTheoSanPham()
+        {
+            return dal.GetDoanhThuTheoSanPham();
+        }
+    }
+}
